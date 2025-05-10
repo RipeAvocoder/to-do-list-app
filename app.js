3e00e06dbf475d8bc3b8d7a4bf3a6c3b1c3daf89
Olli Norgate <53353949+RipeAvocoder@users.noreply.github.com>
2025-05-10 23:03:47 +0100

    RA-3 + RA-4(#2)
    * RA-3 added routes and controllers
    
    * RA-3 + RA-4 combined these tickets and fixed issue with ordering in app.js where app.use (taskRoutes) needed to come after app.use(express.json())

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 require("dotenv").config();
 const connectDB = require('./config/db');
-
+const taskRoutes = require("./routes/tasks");
 const express = require("express");
+
 
 const app = express();
 const PORT = 3000;
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/tasks", taskRoutes);
+
 app.get("/", (req, res) => {
-  // Using send function we send
-  // response to the client
-  // Here we are sending html
   res.send("<h1> Hello World! </h1>");
 });
 
